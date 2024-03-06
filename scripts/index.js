59377958f9632051bf314f52f899fe86db2d2cf0
jakanoh <jakanoh17@gmail.com>
2024-03-06 01:18:09 -0500

    add 'insert new card' logic

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,53 @@ let initialCards = [
   },
 ];
 
-let closeButton = document.querySelector(".modal__close-button");
-let editButton = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal__close-button");
+const editButton = document.querySelector(".profile__edit-button");
+const modal = document.querySelector(".modal");
+const modalNameInput = modal.querySelector(".modal__input_type_name");
+const modalDescriptionInput = modal.querySelector(
+  ".modal__input_type_description"
+);
+const modalSaveButton = document.querySelector(".modal__save-button");
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+const modalForm = document.getElementById("edit-form");
+const cardTemplate = document.querySelector("#card-template");
 
-closeButton.addEventListener("click", function () {
+function closeForm() {
   modal.classList.remove("modal_opened");
-});
+}
+
+closeButton.addEventListener("click", closeForm);
 
 editButton.addEventListener("click", function () {
   modal.classList.add("modal_opened");
+  modalNameInput.value = profileName.textContent;
+  modalDescriptionInput.value = profileTitle.textContent;
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalNameInput.value;
+  profileTitle.textContent = modalDescriptionInput.value;
+  closeForm();
+}
+
+modalForm.addEventListener("submit", handleProfileFormSubmit);
+
+function getCardElement(data) {
+  let cardElement = cardTemplate.cloneNode(true).content;
+  let cardTitle = cardElement.querySelector(".gallery__location");
+  let cardImage = cardElement.querySelector(".gallery__image");
+  cardImage.setAttribute("src", data.link);
+  cardImage.setAttribute("alt", data.name);
+  cardTitle.textContent = data.name;
+  return cardElement.innerHTML;
+}
+
+for (let initialCard of initialCards) {
+  getCardElement(initialCard);
+  document
+    .querySelector(".gallery")
+    .insertAdjacentHTML("afterbegin", cardElement.innerHTML);
+}
